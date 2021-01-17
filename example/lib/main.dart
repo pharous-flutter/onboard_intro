74901a110050711a7cc34fc69981ed349671c752
Ahmed Elsayed <ahmed.elsayed@pharous.com>
2021-01-17 13:27:28 +0200

    Onboarding widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,39 +23,42 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<OnBoardingData> data = [
-    OnBoardingData('images/on_boarding_1.svg',
-        'من خلال سليم ، يراك الدكتور في المنزل ، ويمكنه فهم نمط حياتك بشكل أفضل من خلال متابعة نظامك الغذائي ، مراجعةوصفاتك الطبية و متابعة تاريخك الصحي للمساعدة علي تقيم حالتك بشكل أفضل و تقديم الرعاية المناسبة.',
+    OnBoardingData('images/onboarding1.svg',
+        'First Screen Description...',
         isSvg: true),
     OnBoardingData(
-      'images/on_boarding_2.svg',
-      'محتوى الصفحة الثانية من التعريف بالتطبيق ... محتوى الصفحة الثانية من التعريف بالتطبيق ... محتوى الصفحة الثانية من التعريف بالتطبيق ... محتوى الصفحة الثانية من التعريف بالتطبيق ... محتوى الصفحة الثانية من التعريف بالتطبيق ... ',
+      'images/onboarding2.svg',
+      'Second Screen Description...',
       isSvg: true,
     ),
     OnBoardingData(
-      'images/on_boarding_3.svg',
-      'محتوى الصفحة الثالثة من التعريف بالتطبيق ... محتوى الصفحة الثالثة من التعريف بالتطبيق ... محتوى الصفحة الثالثة من التعريف بالتطبيق ... ',
+      'images/onboarding3.svg',
+      'Third Screen Description...',
       isSvg: true,
     ),
   ];
 
+  var _key;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _key,
       body: OnBoardingIntro(
         isSvgImage: true,
-        selectedColor: Colors.red,
+        selectedColor: Color(0xff35200b),
         unSelectedColor: Colors.grey,
         finishText: 'Home',
         nextText: 'Next',
         backText: 'Back',
         skipText: 'Skip',
         onFinalClicked: () {
-          print('Finished');
+         Scaffold.of(context).showSnackBar(SnackBar(content: Text('Finished')));
         },
         onSkipClicked: () {
-          print('Skiped');
+          Scaffold.of(context).showSnackBar(SnackBar(content: Text('Skiped')));
         },
-        logoImage: 'images/logo_login.svg',
+        logoImage: 'images/logo.svg',
         style: TextStyle(
             fontSize: 17, fontWeight: FontWeight.bold, wordSpacing: 2),
         data: data,
